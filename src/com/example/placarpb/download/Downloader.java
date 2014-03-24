a31014ae849db9fb16fe24b410bb75bd8a61c59d
Matheus Brasileiro Campos <matheusbc@gmail.com>
2014-03-24 02:37:30 -0300

    Storage shield images.

--- src/com/example/placarpb/download/Downloader.java
+++ src/com/example/placarpb/download/Downloader.java
@@ -4,20 +4,24 @@
     import android.graphics.Bitmap;
     import android.graphics.BitmapFactory;
     import android.os.AsyncTask;
+    import android.os.Environment;
     import android.util.Log;
     import android.widget.*;
 
-    import java.io.IOException;
-    import java.io.InputStream;
+    import java.io.*;
     import java.net.HttpURLConnection;
     import java.net.URL;
 
     public class Downloader extends AsyncTask<String, Integer, Bitmap> {
 
-        private ProgressBar mProgressBar;
-        private Context mContext;
-        private ImageView mImageView;
-        private TextView mPercent;
+        private final static int FI_URL_SIZE = 32;
+        private final static String FOLDER = "PlacarPB/images";
+
+        private final ProgressBar mProgressBar;
+        private final Context mContext;
+        private final ImageView mImageView;
+        private final TextView mPercent;
+        private String mUrls;
 
         public Downloader(ProgressBar progressBar, ImageView imageView, TextView percent,
                           Context context) {
@@ -32,6 +36,7 @@
             int count = urls.length;
             Bitmap bmp = null;
             for (int i = 0; i < count; i++) {
+                mUrls = urls[i];
                 bmp = getBitmapFromURL(urls[i]);
                 publishProgress((int) ((i / (float) count) * 100));
                 if (isCancelled()) break;
@@ -53,9 +58,32 @@
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
+            String[] splitUrl = mUrls.split("/");
+            String filename = splitUrl[splitUrl.length - 1];
+            String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+            File folder = new File(extStorageDirectory + "/" + FOLDER);
+            if (createFolder(extStorageDirectory, FOLDER)) {
+                Log.e("OK", "!");
+            }
+            File file = new File(folder, filename);
+
+            if (!file.exists()) {
+                try {
+                    FileOutputStream out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    out.flush();
+                    out.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("HA", "HA");
+            }
+
             if (null != mImageView) {
                 mImageView.setImageBitmap(bitmap);
             }
+
             Toast.makeText(mContext, "download complete", Toast.LENGTH_SHORT).show();
 
             super.onPostExecute(bitmap);
@@ -79,4 +107,18 @@
             }
         }
 
+        private boolean createFolder(String path, String folderName) {
+            String[] foldersName = folderName.split("/");
+            File folder = new File(path + "/" + foldersName[0]);
+            boolean created = false;
+            if (!folder.exists()) {
+                if (folder.mkdir()) {
+                    created = true;
+                }
+            }
+            if (foldersName.length > 1) {
+                created = createFolder(folder.toString(), foldersName[1]);
+            }
+            return created;
+        }
     }
