45a10405f12958471a90c12b5b5273d25b71b73f
Matheus Brasileiro Campos <matheusbc@gmail.com>
2014-03-26 01:06:44 -0300

    Close #5 - Created championship ranking model.

---
+++ src/com/example/placarpb/model/Ranking.java
@@ -0,0 +1,77 @@
+package com.example.placarpb.model;
+
+public class Ranking implements Comparable<Ranking> {
+    private final String mTeamName;
+    private final String mLegendColor;
+    private final int mPoints;
+    private final int mGames;
+    private final int mWins;
+    private final int mGoals;
+    private final int mPosition;
+
+    public Ranking(String teamName, String legendColor, int points, int games, int wins,
+                   int goals, int position) {
+        this.mTeamName = teamName;
+        this.mLegendColor = legendColor;
+        this.mPoints = points;
+        this.mGames = games;
+        this.mWins = wins;
+        this.mGoals = goals;
+        this.mPosition = position;
+    }
+
+    public String getTeamName() {
+        return mTeamName;
+    }
+
+    public String getLegendColor() {
+        return mLegendColor;
+    }
+
+    public int getPoints() {
+        return mPoints;
+    }
+
+    public int getGames() {
+        return mGames;
+    }
+
+    public int getWins() {
+        return mWins;
+    }
+
+    public int getGoals() {
+        return mGoals;
+    }
+
+    public int getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String toString() {
+        return mPosition + " - " + mTeamName + " - " + mPoints + " " + mGames + " " + mWins + " "
+                + mGoals;
+    }
+
+    @Override
+    public int compareTo(Ranking another) {
+        final int comparison;
+        if (mPosition < another.getPosition()) {
+            comparison = 1;
+        } else if (mPosition > another.getPosition()) {
+            comparison = -1;
+        } else if (mWins > another.getWins()) {
+            comparison = 1;
+        } else if (mWins < another.getWins()) {
+            comparison = -1;
+        } else if (mGoals > another.getGoals()) {
+            comparison = 1;
+        } else if (mGoals < another.getGoals()) {
+            comparison = -1;
+        } else {
+            comparison = 0;
+        }
+        return comparison;
+    }
+}
